f6d2cecafe94bc1dd1d5268dc36b26efd689aa97
alexandr <Control de ventanaVista mvil Acerca dealeksandr.tarasov@fyg.ugr.es>
2024-09-14 16:46:18 +0200

    fix(user): duplicate users email error

--- src/main/java/com/tarasov1812/jobportal/controller/UsersController.java
+++ src/main/java/com/tarasov1812/jobportal/controller/UsersController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class UsersController {
@@ -33,7 +34,15 @@ public class UsersController {
     }
 
     @PostMapping("/register/new")
-    public String userRegistration(@Valid Users users){
+    public String userRegistration(@Valid Users users, Model model){
+        Optional<Users> optionalUsers =usersService.getUserByEmail(users.getEmail());
+        if(optionalUsers.isPresent()){
+            model.addAttribute("error", "Email already registered, try to login or register with other email.");
+            List<UsersType> usersType = usersTypeService.getAll();
+            model.addAttribute("getAllTypes", usersType);
+            model.addAttribute("user", new Users());
+            return "register";
+        }
         usersService.addNew(users);
         return "dashboard";
     }
