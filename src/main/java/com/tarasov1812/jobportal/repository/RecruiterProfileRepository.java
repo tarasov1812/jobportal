2ff8dcceb041ca17ed680b1035a93473e462b70f
alexandr <Control de ventanaVista mvil Acerca dealeksandr.tarasov@fyg.ugr.es>
2024-09-15 15:48:11 +0200

    feat(user): create repositories for job seeker and recruiter profile

---
+++ src/main/java/com/tarasov1812/jobportal/repository/RecruiterProfileRepository.java
@@ -0,0 +1,7 @@
+package com.tarasov1812.jobportal.repository;
+
+import com.tarasov1812.jobportal.entity.RecruiterProfile;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecruiterProfileRepository extends CrudRepository<RecruiterProfile, Integer> {
+}
